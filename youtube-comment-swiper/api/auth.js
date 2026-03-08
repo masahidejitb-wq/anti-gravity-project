6dd86aae84424021e27e80d7f8132eae80c2bd46
Hide <masahide.jitb@gmail.com>
2026-03-08 22:57:09 +0900

    Fix OAuth scope - add youtube scope for likes

--- youtube-comment-swiper/api/auth.js
+++ youtube-comment-swiper/api/auth.js
@@ -4,7 +4,7 @@
 module.exports = (req, res) => {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const redirectUri = `${req.headers['x-forwarded-proto'] || 'https'}://${req.headers.host}/api/callback`;
-  const scope = 'https://www.googleapis.com/auth/youtube.force-ssl';
+  const scope = 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtube';
 
   const authUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
   authUrl.searchParams.set('client_id', clientId);
